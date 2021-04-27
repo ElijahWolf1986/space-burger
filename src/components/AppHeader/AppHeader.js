784bcd7f1343ffe45431f053482005342825066a
Ilya Volkov <102101100@mail.ru>
2021-04-27 16:49:01 +0300

    create: AppHeader

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,32 +1,41 @@
 import React from "react";
 
-import { Logo, BurgerIcon, ListIcon, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./AppHeader.module.css";
 
 function AppHeader() {
-
-  
-  
-
   return (
-<>
+    <header className={styles.header}>
 
-<Logo /> 
+      <menu className={styles.menu}>
+        <ul className={styles.menu_list}>
+          <li className={styles.menu_item}>
+            <BurgerIcon type="primary" />
+            <p className={`${styles.menu_title} text text_type_main-default`}>Конструктор</p>
+          </li>
+          <li className={styles.menu_item}>
+            <ListIcon type="secondary" />
+            <p className={`${styles.menu_title} ${styles.menu_title_active} text text_type_main-default`}>Лента заказов</p>
+          </li>
+        </ul>
+        <Logo />
+      </menu>
 
+      {/* <Logo /> */}
 
-<BurgerIcon type="primary" />
-<ListIcon type="primary" />
-<ProfileIcon type="primary" />
+      <profile className={styles.profile}>
+      <ProfileIcon type="secondary" />
+      <p className={`${styles.profile_title} ${styles.profile_title_active} text text_type_main-default`}>Личный кабинет</p>
+      </profile>
 
-
-
-
-
-
-
-</>
-   
+      
+    </header>
   );
-  
 }
 
 export default AppHeader;
