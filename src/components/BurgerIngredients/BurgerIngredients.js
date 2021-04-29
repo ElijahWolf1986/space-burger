2252d084f4170aae7556459b784b84788cf2b60b
Ilya Volkov <102101100@mail.ru>
2021-04-29 13:12:04 +0300

    create: BurgerIngredients

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -1,15 +1,37 @@
 import React from "react";
+// import { useHistory, Link, useLocation } from "react-router-dom";
 import styles from "./BurgerIngredients.module.css";
 import Ingredient from "../Ingredient/Ingredient";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import Data from "../../utils/data";
 
 function BurgerIngredients() {
   const [current, setCurrent] = React.useState("one");
 
+  //   const history = useHistory(); // Была попытка организовть якорную ссылку, но ничего не вышло ()
+  //   const turnToFilling = () => {
+  //     history.push("#filling");
+  //     setCurrent("three");
+  //   };
+  // const {name, fat, price} = Data;
+
+//   отбор захардкоренных данных
+  const bunArr = Data.filter((item) => {
+    return item.type === "bun";
+  });
+  const sauceArr = Data.filter((item) => {
+    return item.type === "sauce";
+  });
+  const fillingArr = Data.filter((item) => {
+    return item.type === "main";
+  });
+//   ************************************
+
   return (
     <section id="BurgerIngredients" className={styles.burger}>
       <h1 className={styles.burger_header}>Соберите бургер</h1>
-      <menu className={styles.burger_menu}>
+      {/* Меню ингредиетов */}
+      <menu className={styles.burger_menu}> 
         <Tab value="one" active={current === "one"} onClick={setCurrent}>
           Булки
         </Tab>
@@ -20,10 +42,53 @@ function BurgerIngredients() {
           Начинки
         </Tab>
       </menu>
+      {/* *************** */}
       <section className={styles.ingredients}>
-
-        <Ingredient />
-
+        {/* Отображение булок */}
+        <div id="bun" className={styles.ingredients_bun}>
+          <p className={styles.ingredients_title}>Булки</p>
+          <div className={styles.ingredients_list}>
+            {bunArr.map((item) => {
+              return (
+                <Ingredient
+                  image={item.image}
+                  name={item.name}
+                  price={item.price}
+                />
+              );
+            })}
+          </div>
+        </div>
+        {/* Отображение Соусов */}
+        <div id="sauce" className={styles.ingredients_sauce}>
+          <p className={styles.ingredients_title}>Соусы</p>
+          <div className={styles.ingredients_list}>
+            {sauceArr.map((item) => {
+              return (
+                <Ingredient
+                  image={item.image}
+                  name={item.name}
+                  price={item.price}
+                />
+              );
+            })}
+          </div>
+        </div>
+        {/* Отображение Начинок */}
+        <div id="filling" className={styles.ingredients_filling}>
+          <p className={styles.ingredients_title}>Начинки</p>
+          <div className={styles.ingredients_list}>
+            {fillingArr.map((item) => {
+              return (
+                <Ingredient
+                  image={item.image}
+                  name={item.name}
+                  price={item.price}
+                />
+              );
+            })}
+          </div>
+        </div>
       </section>
     </section>
   );
--- src/components/Ingredient/Ingredient.js
+++ src/components/Ingredient/Ingredient.js
@@ -5,29 +5,20 @@ import {
   Counter,
   CurrencyIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
-import { isPropertySignature } from "typescript";
 
-function Ingredient() {
+
+function Ingredient(props) {
   return (
     <section className={styles.ingredient}>
-        <Counter count={1} size="default" />
-      {/* <img src={props.image} alt={props.name} />
-            <p className={styles.ingredient_price}>
-                {props.price}
-                
-            </p>
-            <p className={styles.ingredient_name}>{props.name}</p> */}
+      <Counter count={1} size="small" />
+      <img src={props.image} alt={props.name} />
 
-      <img
-        src="https://code.s3.yandex.net/react/code/bun-02.png"
-        alt="Краторная булка N-200i"
-      />
-      <p className={styles.ingredient_price}>
-        1255
+      <div className={styles.ingredient_price}>
+        <p className={styles.ingredient_price_value}>{props.price}</p>
+
         <CurrencyIcon />
-      </p>
-      <p className={styles.ingredient_name}>"Краторная булка N-200i"</p>
-
+      </div>
+      <p className={styles.ingredient_name}>{props.name}</p>
     </section>
   );
 }
